c7d2ae902a3a3ca8fdebd46b437af0a5e0487b25
Ali Baba <alibabatorjan001@gmail.com>
2018-11-09 02:00:59 +0500

    added Linking between  routes

--- src/app.js
+++ src/app.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import ReactDOM from "react-dom";
-import { BrowserRouter, Route, Switch } from 'react-router-dom';
+import { BrowserRouter, Route, Switch, Link ,NavLink} from 'react-router-dom';
 import './styles/styles.scss'
 import 'normalize.css/normalize.css'
 
@@ -50,8 +50,22 @@ const PageNotFound = () => (
 
 );
 
+
+const Header = () => (
+    <header>
+        <h1>Expenisfy </h1>
+        <NavLink to="/" activeClassName="is-active" exact={true}>Dashboard</NavLink>
+        <NavLink to="/create" activeClassName="is-active">Create page</NavLink>
+        <NavLink to="/edit" activeClassName="is-active">Edit page</NavLink>
+        <NavLink to="/help" activeClassName="is-active"> Help page</NavLink>
+    </header>
+);
+
 const route = (
+
     <BrowserRouter>
+        <div>
+        <Header /> 
         <Switch>//  now 404 will fallback to index.html
             <Route path="" component={ExpenseDashboardPage} exact={true} />
             <Route path="/create" component={AddExpensePage} />
@@ -59,6 +73,7 @@ const route = (
             <Route path="/help" component={HelpPage} />
             <Route component={PageNotFound} />
         </Switch>
+        </div>
     </BrowserRouter>
 
 
