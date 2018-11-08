5f0ec143d063db0f7ce3f540887f15137e125d17
Ali Baba <alibabatorjan001@gmail.com>
2018-11-09 01:06:03 +0500

    bug or feature: 404 will fallback to /index.html (this is associated with webpack) , I also added Switch of React Router

--- src/app.js
+++ src/app.js
@@ -1,16 +1,16 @@
 import React from 'react';
 import ReactDOM from "react-dom";
-import {BrowserRouter, Route} from 'react-router-dom';
+import { BrowserRouter, Route, Switch } from 'react-router-dom';
 import './styles/styles.scss'
 import 'normalize.css/normalize.css'
 
 
 
-const ExpenseDashboardPage = () =>(
+const ExpenseDashboardPage = () => (
 
     <div>
-    <p>This is dashboard page </p>
-    
+        <p>This is dashboard page </p>
+
     </div>
 
 );
@@ -28,33 +28,41 @@ const AddExpensePage = () => (
 const EditExpensePage = () => (
 
     <div>
-    
-    <p>This is Edit page</p>
+
+        <p>This is Edit page</p>
     </div>
 );
 const HelpPage = () => (
-<div>
+    <div>
 
-<p>
-This is help component
+        <p>
+            This is help component
 </p>
 
-</div>
+    </div>
 );
 
 
+const PageNotFound = () => (
+    <div>
+        <p>404!</p>
+    </div>
+
+);
+
 const route = (
     <BrowserRouter>
-        <div>
-        <Route path="" component={ExpenseDashboardPage} />
-        <Route path="/create" component={AddExpensePage} />
-        <Route path="/edit" component={EditExpensePage}/>
-        <Route path="/help" component={HelpPage}/>
-    </div>
+        <Switch>//  now 404 will fallback to index.html
+            <Route path="" component={ExpenseDashboardPage} exact={true} />
+            <Route path="/create" component={AddExpensePage} />
+            <Route path="/edit" component={EditExpensePage} />
+            <Route path="/help" component={HelpPage} />
+            <Route component={PageNotFound} />
+        </Switch>
     </BrowserRouter>
 
 
 );
 
-ReactDOM.render(route , document.getElementById('app'));
+ReactDOM.render(route, document.getElementById('app'));
 
--- webpack.config.js
+++ webpack.config.js
@@ -5,7 +5,7 @@ module.exports = {
 
     entry: './src/app.js',
     output:{
-        path:path.join(__dirname,'public'),
+        path:path.join(__dirname,"public"),
         filename: 'bundle.js'
     },
 
